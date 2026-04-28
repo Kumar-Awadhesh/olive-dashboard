256255544d2c3662e0352895611cf5dcb3bd1ec2
Awadhesh Kumar <kumaravi0506@gmail.com>
2026-04-29 03:26:05 +0530

    almost finished!

--- vite-project/src/components/dashboard.jsx
+++ vite-project/src/components/dashboard.jsx
@@ -1,15 +1,31 @@
-import { useContext } from "react";
+import { useContext, useState, useEffect, use } from "react";
 import { DashboardContext } from "../contexts/dashboardContext";
 
 
 
 export const Dashboard = () => {
 
-    const {navbarBtn, setNavbarbtn, downloadBtn, setDownloadBtn} = useContext(DashboardContext);
+    const { navbarBtn, setNavbarbtn, downloadBtn, setDownloadBtn } = useContext(DashboardContext);
+    const [image, setImage] = useState(0);
 
 
+    const images = [
+        "/images/product.webp",
+        "/images/product.webp",
+        "/images/product.webp",
+        "/images/product.webp",
+        "/images/product.webp"
+    ]
 
-    return(
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setImage((prev) => (prev + 1) % images.length)
+        }, 5000)
+        return () => clearInterval(interval);
+    }, [])
+
+
+    return (
         <>
             <main className="main-Container">
                 <nav className="navbar">
@@ -38,6 +54,32 @@ export const Dashboard = () => {
                 <div className="shop-tag-container">
                     <h4>The Safest Way to</h4>
                     <h4>Shop for Groceries</h4>
+                </div>
+                <p className="olive-para">
+                    Use the Olive Food Scanner App to Instantly Eliminate Harmful Ingredients
+                    from Your Family's Diet and Get Expert-Backed Food Insights
+                </p>
+                <div className="downloadBtn-container">
+                    <button>Download for iOS</button>
+                    <button>Join the Olive Community</button>
+                </div>
+                <div className="mobile-grocery-container">
+                    <div className="mobile-container">
+                        <div className="camera-container">
+                            <div></div>
+                        </div>
+                        <div className="grocery-container">
+                            <div className="product-img-container"
+                                style={{ transform: `translateX(-${image * 100}px)` }}
+                            >
+                                {
+                                    images.map((img, index) => (
+                                        <img className={index-2 === image ? "main-product-img" : "product-img" } key={index} src={img} />
+                                    ))
+                                }
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </main>
         </>
